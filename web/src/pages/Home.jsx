f3bdff2d854d7169e50e7ed553b83e78655ddea8
riwu <riwu0730@gmail.com>
2018-07-10 09:50:46 +0800

    use Alert for login warning

--- web/src/pages/Home.jsx
+++ web/src/pages/Home.jsx
@@ -1,45 +1,53 @@
 import React from 'react';
+import { Alert } from 'antd';
 import Anchor from '../components/Anchor';
 import { AppStore, GooglePlay, AppIcon } from '../assets/images';
 import Login from '../components/Login';
 import styles from './Home.module.css';
 
 const Home = props => (
-  <div className={styles.container}>
-    <Login
-      className={styles.login}
-      onLogin={() => {
-        if ((props.location.state || {}).redirected) {
-          props.history.goBack();
-        } else {
-          props.history.push('/classes');
-        }
-      }}
-    />
-    <AppIcon />
-    <h1>Welcome to SLIONS</h1>
-    <h2>
-      A multi-language Karaoke app for making language learning fun and engaging by providing
-      automated feedback through speech to text transcription as well as personalised profile and
-      usage analysis.
-    </h2>
-    <h2>
-      Awarded{' '}
-      <Anchor href="https://comp.nus.edu.sg/entrepreneurship/awards/winners">
-        I&E Practicum Grant ($10000) and SoC Innovation Prize ($2000) 2018
+  <React.Fragment>
+    {(props.location.state || {}).redirected && (
+      <div className={styles.alertContainer}>
+        <Alert message="Please login first!" type="info" showIcon />
+      </div>
+    )}
+    <div className={styles.container}>
+      <Login
+        className={styles.login}
+        onLogin={() => {
+          if ((props.location.state || {}).redirected) {
+            props.history.goBack();
+          } else {
+            props.history.push('/classes');
+          }
+        }}
+      />
+      <AppIcon />
+      <h1>Welcome to SLIONS</h1>
+      <h2>
+        A multi-language Karaoke app for making language learning fun and engaging by providing
+        automated feedback through speech to text transcription as well as personalised profile and
+        usage analysis.
+      </h2>
+      <h2>
+        Awarded{' '}
+        <Anchor href="https://comp.nus.edu.sg/entrepreneurship/awards/winners">
+          I&E Practicum Grant ($10000) and SoC Innovation Prize ($2000) 2018
+        </Anchor>
+      </h2>
+      <br />
+      <h3>
+        This is the web portal for teachers.<br />Download the mobile app now!
+      </h3>
+      <Anchor className={styles.appStore} href="https://itunes.apple.com/app/id1328375727">
+        <AppStore href={props.href} />
       </Anchor>
-    </h2>
-    <br />
-    <h3>
-      This is the web portal for teachers.<br />Download the mobile app now!
-    </h3>
-    <Anchor className={styles.appStore} href="https://itunes.apple.com/app/id1328375727">
-      <AppStore href={props.href} />
-    </Anchor>
-    <Anchor href="https://play.google.com/store/apps/details?id=riwu.slions">
-      <GooglePlay href={props.href} />
-    </Anchor>
-  </div>
+      <Anchor href="https://play.google.com/store/apps/details?id=riwu.slions">
+        <GooglePlay href={props.href} />
+      </Anchor>
+    </div>
+  </React.Fragment>
 );
 
 export default Home;
--- web/src/routes/PrivateRoute.jsx
+++ web/src/routes/PrivateRoute.jsx
@@ -1,14 +1,12 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
-import { message } from 'antd';
 
 const PrivateRoute = ({ component: Component, isAuthenticated, ...rest }) => (
   <Route
     {...rest}
     render={(props) => {
       if (isAuthenticated) return <Component {...props} />;
-      message.info('Please login first!');
       return (
         <Redirect
           push
