58e7deec226b4a23ba6a21ac04cfeebfc1c60a44
riwu <riwu0730@gmail.com>
2018-07-15 19:50:30 +0800

    replace alert with notification for private route redirect

--- web/src/routes/PrivateRoute.jsx
+++ web/src/routes/PrivateRoute.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Route, Redirect } from 'react-router-dom';
+import { notification } from 'antd';
 
 const PrivateRoute = ({ component: Component, isAuthenticated, ...rest }) => (
   <Route
     {...rest}
     render={(props) => {
       if (isAuthenticated) return <Component {...props} />;
+      notification.info({ message: 'Please login first!', placement: 'topLeft', duration: 0 });
       return (
         <Redirect
           push
