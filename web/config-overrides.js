3acd6eabde23e68ac0d04aeb8e6f34e8314a9b5c
riwu <riwu0730@gmail.com>
2018-09-02 12:06:04 +0800

    add react-app-rewire-less to customize antd theme

--- web/config-overrides.js
+++ web/config-overrides.js
@@ -1,11 +1,18 @@
 const { injectBabelPlugin } = require('react-app-rewired');
 const rewireReactHotLoader = require('react-app-rewire-hot-loader');
+const rewireLess = require('react-app-rewire-less');
 
 module.exports = function override(config, env) {
   config = injectBabelPlugin(
-    ['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }],
+    ['import', { libraryName: 'antd', libraryDirectory: 'es', style: true }],
     config,
   );
+
+  config = rewireLess.withLoaderOptions({
+    modifyVars: { '@primary-color': '#6c6ed7' },
+    javascriptEnabled: true,
+  })(config, env);
+
   config =
     env === 'production'
       ? injectBabelPlugin(['transform-remove-console'], config)
